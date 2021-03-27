1c88565a1ebbfdc5e3eb3ea13fecd457fd0e6b32
Egils Consulting <egils@riseup.net>
2021-03-27 22:11:39 +0100

    förbättra geolocation modellen med area name möjliggör val av geopunkt för observation lägg till label för nuvarande område och gpspunkt

--- Databas_VictorSmith_C-sharp/Models/Geolocation.cs
+++ Databas_VictorSmith_C-sharp/Models/Geolocation.cs
@@ -10,5 +10,10 @@ namespace Databas_VictorSmith_C_sharp.Models
         public double Longitude { get; set; }
         public double Latitude { get; set; }
         public int Area_Id { get; set; }
+        public string Area_Name { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0}: {1},{2}", Area_Name, Latitude, Longitude);
+        }
     }
 }
